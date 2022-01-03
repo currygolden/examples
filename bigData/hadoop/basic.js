ea48ee89b4bc866bad0f4fafb34ae6adddbbb569
liyu38 <liyu38@meituan.com>
2022-01-03 23:29:29 +0800

    update bigData

---
+++ bigData/hadoop/basic.js
@@ -0,0 +1,15 @@
+/**
+ * 1. hadoop由 辅助工具 数据存储HDFS 资源调度 数据计算
+ * 2. HDFS 是分布式文件存储系统
+ *    NameNode: 数据存储在什么位置
+ *    DataNode: 具体存储数据的节点
+ *    2NN: NameNode 辅助记录，相当于数据备份
+ * 3. YARN: 资源管理器
+ *    resourceManager: 整个集群资源（内存，cpu）管理
+ *    nodeManager: 单个节点服务器资源管理
+ *    applicationMaster: 描述单个任务
+ *    容器： 类似于docker?
+ * 4. mapReduce: 数据的检索与输出
+ * 5. 描述 HDFS，YARN，mapReduce的工作流程
+ * 
+  */

0ffdab58248ba603a9b1fc29a471fb3acc5bb9e2
liyu <liyu@meituan.com>
2022-01-07 10:34:16 +0800

    update

---
+++ bigData/Docker/basic.js
@@ -0,0 +1,6 @@
+/**
+ * 1. https://hub.docker.com/ 以及 docker客户端
+ * 2. image：Docker 把应用程序及其依赖，打包在 image 文件里面。可以当作class,实现继承等
+ * 3. container: 在容器里运行 image，容器实例，本身也是一个文件，称为容器文件
+ * 4. container 其实在做进程隔离，需要把本机端口，本机shell映射过去
+ */

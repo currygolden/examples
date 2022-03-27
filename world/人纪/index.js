6e7fe327d178c054c4012d213467854557962ba7
liyu38 <liyu38@meituan.com>
2022-03-27 22:49:40 +0800

    update

---
+++ world/人纪/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-26 13:27:43
+ * @LastEditTime: 2022-03-26 22:04:48
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /examples/world/人纪/index.js
+ */
+/**
+ * @description: 
+
+ */
---
+++ world/天玑/index1.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-23 23:24:43
+ * @LastEditTime: 2022-03-24 01:13:31
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /examples/world/天玑/index1.js
+ */
+/**
+ * 1. 天纪：代表真理，已经被历史所证实
+ * 2. 关注事情的神而不是形，后者是无穷尽的，有涯随无涯则殆
+ *  2.1 形可以无限复杂而耗费时间甚至内卷，传神，用神才靠近本质和真理
+ *  2.3 神代表本质的道理，即易和禅，都是简单和简洁，背后是真的懂了
+ *  2.4 童蒙和成蒙是差别很大的，思考后再去拒绝，批评，而不要一开始带着成见
+ * 3. 紫薇斗数
+ *  3.1 天地人三才，天是命，地是势，可以后天下功夫调整
+ *  3.2 斗数：星斗的计算结果
+ *  3.3 命宫 财帛 官禄 迁徙（顺势而为，否则再回首已是百年身）
+ */

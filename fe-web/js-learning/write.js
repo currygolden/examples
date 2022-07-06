36f34d95ed3c17098563b6d9641ffc0cecb858b8
liyu38 <liyu38@meituan.com>
2022-07-06 22:18:28 +0800

    feat: update read

--- fe-web/js-learning/write.js
+++ fe-web/js-learning/write.js
@@ -31,3 +31,72 @@
  *
  *
  */
+
+
+
+
+
+
+Function.prototype.bind2 = function(context, ...args) {
+  const that = this
+
+  const resFn = function(...args2) {
+    if (this instanceof resFn) {
+      return new that(...args, ...args2)
+    }
+    return that.apply(context, [...args, ...args2])
+  }
+
+  return resFn
+}
+
+function shimNew(construc, ...args) {
+  const obj = Object.create(null)
+  obj.__proto__ === construc.prototype
+  const res = construc.apply(obj, args)
+
+  return typeof res === 'object' ? res : obj
+}
+
+/**
+ * @description 实现观察者模式
+ * 1. addListener(event, listener)
+ * 2. removeListener(event, listener)
+ * 3. setMaxListeners(n)
+ * 4. once(event, listener)
+ * 5. emit(event, [arg1], [arg2], [...])
+ * 关注点在于event
+ * map 结构存储event&回调数组
+ * 
+  */
+ function EventEmitter() {
+   this._maxListeners = 10
+   this.events = Object.create(null)
+ }
+
+EventEmitter.prototype.addListener = function(type, listener, prepend) {
+  if (this.events[type] && Array.isArray(this.events[type])) {
+    if (prepend) {
+      this.events[type].unshift(listener)
+    } else {
+      this.events[type].push(listener)
+    }
+  } else {
+    this.events[type] = [listener]
+  }
+}
+
+EventEmitter.prototype.removeListener = function(type, listener) {
+  if (Array.isArray(this.events[type])) {
+    if (!listener) {
+      delete this.events[type]
+    } else {
+      this.events[type].filter(item => item !== listener)
+    }
+  }
+}
+
+EventEmitter.prototype.once = function(type, listener) {
+  
+}
+

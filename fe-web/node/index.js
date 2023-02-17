9e3681a00df9f1211ab160de5836d92366128c29
liyu <liyu38@meituan.com>
2023-02-17 20:07:44 +0800

    feat: update info

--- fe-web/node/index.js
+++ fe-web/node/index.js
@@ -1,4 +1,27 @@
 /**
  * @description 参考阅读资料
  * 1. https://www.nodejs.red/#/nodejs/base/what-is-nodejs
+ * 2. https://www.bookstack.cn/read/Nodejs-Roadmap/_coverpage.md
+ */
+
+
+
+/**
+ * @description 项目实践
+ * 1. https://github.com/Imfdj/egg-beehive/blob/main/README.md
+ *
+ */
+
+
+
+
+
+
+/**
+ * @description:数据库设计
+ * 1. 在开始具体的curd业务之前，先要准备数据
+ *   1.1 使用mysql初始化数据库，提供sql建立表结构
+ *   1.2 在egg-Sequelize背景下完善数据库初始化
+ * 2. 业务场景分析，具体接口设计
+ *
  */
